bf8ac2f666b908df8057023989e431ca69fa7a65
Mike Charo <michaelcharojr@gmail.com>
2026-04-20 23:10:16 -0500

    chore: delete workflow builder demo + retire demo-era docs

--- src/app/builder/page.tsx
+++
@@ -1,9 +0,0 @@
-import WorkflowBuilder from '@/components/workflow/WorkflowBuilder'
-
-export default function BuilderPage() {
-  return (
-    <main className="h-screen w-full">
-      <WorkflowBuilder />
-    </main>
-  )
-}
